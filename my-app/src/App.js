80596b401cc54141f674c5c770d50c0dbce6d34b
Stian Lystad <Stianlystad_1@hotmail.com>
2022-09-21 11:19:03 +0000

    react forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,17 @@ import css from "./App.module.css";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
 import Content from "./components/Content";
+import ControlledForm from "./components/ControlledForm";
+import UncontrolledForm from "./components/UncontrolledForm";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <NavBarForm />
-      <Sidebar />
-      <Content />
+      {/* <NavBarForm /> */}
+      {/* <Sidebar /> */}
+      {/* <Content /> */}
+      <UncontrolledForm />
     </div>
   );
 }
